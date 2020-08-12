9943e3215cc9904ff7f6a246552b8889e5daaaff
Utkarsh Jain <utkarsh.jain@yash.com>
2020-08-12 14:50:07 +0530

    Checking dependancy for junit with failed test case

---
+++ StringCalculatorTDD/src/com/app/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.app.stringcalculator;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class StringCalculatorTest {
+	
+	@Test
+	public void failTest() {
+		fail();
+	}
+
+}
