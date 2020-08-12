f9250eceac4353b732a0df8de5a5623a738ec946
Utkarsh Jain <utkarsh.jain@yash.com>
2020-08-12 16:26:20 +0530

    Test for Empty String

---
+++ StringCalculatorTDD/src/com/app/stringcalculator/StringCalculator.java
@@ -0,0 +1,13 @@
+package com.app.stringcalculator;
+
+public class StringCalculator {
+	
+	public int Add(String input) {
+		int output = -1;
+		if(input.isEmpty()) {
+			output= 0;
+		}
+		return output;
+	}
+
+}
--- StringCalculatorTDD/src/com/app/stringcalculator/StringCalculatorTest.java
+++ StringCalculatorTDD/src/com/app/stringcalculator/StringCalculatorTest.java
@@ -1,14 +1,26 @@
 package com.app.stringcalculator;
 
-import static org.junit.Assert.fail;
 
+import static org.junit.Assert.*;
+
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 	
+	@SuppressWarnings("deprecation")
 	@Test
-	public void failTest() {
-		fail();
+	public void returnZeroWhenEmptyStringIsProvided() {
+		//Given
+		StringCalculator calculator = new StringCalculator();
+		String input = "";
+		//when
+		int output = calculator.Add(input);
+		//Then
+		assertThat(output,Matchers.equalTo(0));
+		
 	}
+	
+	
 
 }
