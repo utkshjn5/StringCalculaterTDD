f029fa1ec5254b0b722b50b724af258da1bb67c6
Utkarsh Jain <utkarsh.jain@yash.com>
2020-09-21 17:23:11 +0530

    test

--- StringCalculatorTDD/src/com/app/stringcalculator/StringCalculator.java
+++ StringCalculatorTDD/src/com/app/stringcalculator/StringCalculator.java
@@ -1,23 +1,54 @@
 package com.app.stringcalculator;
 
-public class StringCalculator {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+/*
+ * public class StringCalculator {
+ * 
+ * public int Add(String input) { String [] arr = input.split(",");
+ * if(input.isEmpty()) { return 0; }else if(input.length()==1){ return
+ * convertStringToInteger(input); }else { return getSum(arr[0],arr[1]); } }
+ * 
+ * public int convertStringToInteger(String input) { return
+ * Integer.parseInt(input); }
+ * 
+ * public int getSum(String value1,String value2) { return
+ * convertStringToInteger(value1)+convertStringToInteger(value2); } }
+ */
+class StringCalculator  {
+	 public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		 String test = "{Test{Test2}{Test3{test4}}}";
+		 List<String> t = data(test,2);
+	}
 	
-	public int Add(String input) {
-		String [] arr = input.split(",");
-		if(input.isEmpty()) {
-			return 0;
-		}else if(input.length()==1){
-			return convertStringToInteger(input);
-		}else {
-			return getSum(arr[0],arr[1]);
+	
+	public static List<String> data(String l,int k){
+		Stack<Character> s = new Stack<>();
+		String[] arr = l.split("\\{");
+		List<String> st = new ArrayList<>();
+		int count=0;
+		int v = 0;
+		for(int i=0;i<l.length();i++) {
+			char ch = l.charAt(i);
+			if(ch=='{') {
+				s.push(ch);
+				count++;
+				v++;
+			}
+			if(ch=='}') {
+				s.pop();
+			}
+			if(count==k) {
+				System.out.println(arr[v]);
+				st.add(arr[v]);
+				count--;
+			}
 		}
-	}
-	
-	public int convertStringToInteger(String input) {
-		return Integer.parseInt(input);
-	}
-	
-	public int getSum(String value1,String value2) {
-		return convertStringToInteger(value1)+convertStringToInteger(value2);
+		
+		return st;
 	}
 }
--- StringCalculatorTDD/src/com/app/stringcalculator/StringCalculatorTest.java
+++ StringCalculatorTDD/src/com/app/stringcalculator/StringCalculatorTest.java
@@ -1,53 +1,10 @@
 package com.app.stringcalculator;
 
 
-import static org.junit.Assert.*;
-
-import org.hamcrest.Matchers;
-import org.junit.Before;
-import org.junit.Test;
-
-public class StringCalculatorTest {
-	StringCalculator calculator;
-	
-	@Before
-	public void intialize() {
-		calculator = new StringCalculator();
-	}
-	@SuppressWarnings("deprecation")
-	@Test
-	public void returnZeroWhenEmptyStringIsProvided() {
-		//Given
-		StringCalculator calculator = new StringCalculator();
-		String input = "";
-		//when
-		int output = calculator.Add(input);
-		//Then
-		assertThat(output,Matchers.equalTo(0));
-	}
-	
-	@SuppressWarnings("deprecation")
-	@Test
-	public void returnSameValueWhenSingleStringIsProvided() {
-		//Given
-		String input = "1";
-		//when
-		int output = calculator.Add(input);
-		//Then
-		assertThat(output,Matchers.equalTo(1));
-	}
-	
-	@SuppressWarnings("deprecation")
-	@Test
-	public void whenTwoCommaSeperatedValuesProvidedThenReturnSum() {
-		//Given
-		String input = "1,2";
-		//when
-		int output = calculator.Add(input);
-		//Then
-		assertThat(output,Matchers.equalTo(3));
-	}
-	
-	
-
+class Base {
+    public void Base() {
+        System.out.println("Base");
+    }
 }
+
+ 
